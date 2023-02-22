94dd283294dac482f86e4c4d8ffcf0576bc5558d
Gustavo Caetano <gustavo.caetano13@gmail.com>
2023-02-22 11:20:05 -0300

    Filtra chaves da api - Parte 2

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ const fetchApi = (value) => {
   return result;
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode'];
+
+const buildResult = (result) => {
+  const newObject = {};
+  keys.map((key) => document.getElementById(key))
+    .map((elem) => {
+      elem.checked && (newObject[elem.name] = result[elem.name]);
+    });
+
+  return newObject;
+}
+
 btnGo.addEventListener('click', async (event) => {
   event.preventDefault();
   const result = await fetchApi(characterId.value);
-  content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+  // content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+  content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
+  console.log(buildResult(result));
   image.src = `${result.image}`;
 });
